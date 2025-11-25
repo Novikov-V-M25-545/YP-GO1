0b90cd1b1dc2da395e08a5dc50d977636980d4b1
Novikov <tutsnew@yandex.ru>
2025-11-25 22:43:39 +0300

    Fix27

--- main.go
+++ main.go
@@ -80,7 +80,8 @@ func parseStats(data string) (*ServerStats, error) {
 
 		if bandwidthUsagePercent > 90 {
 			freeBandwidthBytes := values[5] - values[6]
-			stats.NetworkBandwidth = freeBandwidthBytes / 1_000_000 / 8
+			// Байты/сек → Мегабиты/сек (Мегабайт/сек = Мегабит/сек в этом контексте)
+			stats.NetworkBandwidth = freeBandwidthBytes / 1_000_000
 		}
 	}
 
